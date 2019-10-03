25bacf6d0d04bbe997ec74ad3d5033e7aeac724a
kasdani <42958421+kasdani@users.noreply.github.com>
2019-10-03 20:38:49 +0700

    Add files via upload

---
+++ conf/Config.php
@@ -0,0 +1,7 @@
+<?php
+define("DB_HOST","localhost");
+define("DB_USER","id11097018_kasdanipriyatna");
+define("DB_PASSWORD","duhpohoey");
+define("DB_NAME","id11097018_kasdanipriyatna");
+
+// echo DB_HOST;
